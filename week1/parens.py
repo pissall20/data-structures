b0b5dfecb276aefec29b028db7f6b94381133377
Siddhesh Pisal <pisalsiddhesh@gmail.com>
2020-07-19 10:59:08 +0530

    Completed with correctness

--- week1/parens.py
+++ week1/parens.py
@@ -13,19 +13,25 @@ def find_mismatch(text):
     opening_brackets_stack = []
     for i, next_char in enumerate(text):
         if next_char in "([{":
-            opening_brackets_stack.append(next_char)
-
+            # Process opening bracket, write your code here
+            bracket = Bracket(next_char, i)
+            opening_brackets_stack.append(bracket)
         if next_char in ")]}":
-            if len(opening_brackets_stack) > 0 and are_matching(opening_brackets_stack[-1], next_char):
-                opening_brackets_stack.pop()
-            else:
+            # Process closing bracket, write your code here
+            if not opening_brackets_stack:
                 return i + 1
+            top = opening_brackets_stack.pop()
+            if not are_matching(top.char, next_char):
+                return i + 1
+    if opening_brackets_stack:
+        return opening_brackets_stack[0].position + 1
     return 0
 
 
 def main():
     text = input()
     mismatch = find_mismatch(text)
+    # Printing answer, write your code here
     if mismatch == 0:
         print("Success")
     else:
